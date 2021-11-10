d9cf3681104b5dc1a6889a16ecc7cd52d72ac5de
nadyagrsmva1 <ger.na@stud.s-vfu.ru>
2021-11-11 00:54:45 +0900

    commit

---
+++ src/Primer1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Primer1 {
+    public static void readAllByByte(InputStream in) throws IOException {
+        while (true) {
+            int oneByte = in.read();
+            if(oneByte != -1) {
+                System.out.println((char) oneByte);
+            } else {
+                System.out.println("\n" + "end");
+                break;
+            }
+        }
+    }
+}
---
+++ src/Primer1_2.java
@@ -0,0 +1,18 @@
+// С потоком связана интернет-страница
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Primer1_2 {
+    public static void main(String[] args) {
+        try {
+            InputStream inUrl = new URL("http://google.com").openStream();
+            Primer1.readAllByByte(inUrl);
+            System.out.print("\n\n\n");
+            inUrl.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка: " + e);
+        }
+    }
+}
